505cb02f1f85c714a29d95db2a71d835b1138d2f
Ariste Welmant <ariste.welmant@epfedu.fr>
2024-12-09 21:07:20 +0100

    Jeu fonctionnel complet (condition de victoire + nb de tours pour gagner) : ajout du compte des diagonalles au nb de tour et pour gagner).

--- miniProjet/src/miniprojet/Interface_Lights_Off.java
+++ miniProjet/src/miniprojet/Interface_Lights_Off.java
@@ -171,11 +171,17 @@ public class Interface_Lights_Off extends javax.swing.JFrame {
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
         grille.activerDiagonaleDescendante();
         repaint();
+        nbCoups++;
+        rafraichirGrille();
+        verifierConditionVictoire();
     }//GEN-LAST:event_jButton1ActionPerformed
 
     private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
         grille.activerDiagonaleMontante();
         repaint();
+        nbCoups++;
+        rafraichirGrille();
+        verifierConditionVictoire();
     }//GEN-LAST:event_jButton2ActionPerformed
 
     /**
